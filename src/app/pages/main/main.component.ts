fc844d4d046db9fdbd39a9e7dfa2491c1d7e2ee8
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-09 20:52:19 +0500

    sasas

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -5,6 +5,7 @@ import { RegistrComponent } from "../../Components/registr/registr.component";
 import { FootersComponent } from "../../Components/footers/footers.component";
 import { NavbarComponent } from "../../Components/navbar/navbar.component";
 import { NumbersComponent } from "../../Components/numbers/numbers.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-main',
