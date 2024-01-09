e92338b91a9fa468278b2f1ae4ff23731ba69031
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-09 20:52:10 +0500

    sasa

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from "../../Components/navbar/navbar.component";
 import { FootersComponent } from "../../Components/footers/footers.component";
+import { AddressDate } from '../../models/Address';
+import { AddressService } from '../../services/AddressService/address.service';
+import { QuestionData } from '../../models/Questions';
+import { QuestionsService } from '../../services/Questions/questions.service';
+import {  FormGroup,FormBuilder } from '@angular/forms';
 
 @Component({
     selector: 'app-contact',
@@ -9,6 +14,41 @@ import { FootersComponent } from "../../Components/footers/footers.component";
     styleUrl: './contact.component.scss',
     imports: [NavbarComponent, FootersComponent]
 })
-export class ContactComponent {
 
-}
+
+
+    
+export class ContactComponent implements OnInit {
+    questionsform!: FormGroup;
+    data! : AddressDate;
+    quest!:QuestionData;
+    constructor(
+        private service: AddressService,
+        private question:QuestionsService,
+        private formGroup: FormGroup){
+      
+    }
+  
+    ngOnInit() {
+      this.getAllAddreses()
+      // this.Address = {
+      //   PhoneNumber: '123'
+      // }
+    }
+  
+    getAllAddreses(){
+      this.service.getAllAddress().subscribe(
+        (data:AddressDate) => {
+          this.data = data
+        })
+      
+    }
+
+    addQuestion(){
+
+    }
+  
+  }
+  
+  
+  
