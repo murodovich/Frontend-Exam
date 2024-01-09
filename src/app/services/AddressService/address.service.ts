1c94d1fb45ee09459ffd6261ed786c6bef6ab8c4
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-09 20:52:30 +0500

    sasas

---
+++ src/app/services/AddressService/address.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AddressDate } from '../../models/Address';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AddressService {
+
+  constructor(private http: HttpClient) { 
+
+  }
+
+  getAllAddress(): Observable<AddressDate>{
+    return this.http.get<AddressDate>("http://localhost:5050/api/Address?Id=3")
+  }
+}
