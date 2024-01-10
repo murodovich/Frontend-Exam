8b2f380fdca6d1dbc4bbb8ccfa90b3555eb8432e
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-10 21:49:10 +0500

    saves

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
