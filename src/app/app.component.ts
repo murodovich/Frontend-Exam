328d459cc09ac6b7bb7966192c9e9c7a6faa9c4f
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-10 17:16:13 +0500

    Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,MatToolbarModule,MatIconModule,MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { MainComponent } from './pages/main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'main',component:MainComponent},
+    {path:'**', component:NotFoundComponent}
+];
