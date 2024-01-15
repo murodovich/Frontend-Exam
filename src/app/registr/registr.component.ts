43e48e7c28d5e158ac1f1b126ef8d942d37e0f7f
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-15 15:51:15 +0500

    save

---
+++ src/app/registr/registr.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registr',
+  standalone: true,
+  imports: [],
+  templateUrl: './registr.component.html',
+  styleUrl: './registr.component.scss'
+})
+export class RegistrComponent {
+
+}
