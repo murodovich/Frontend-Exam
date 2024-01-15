81a343602ed83ad5e1329a233fa8eb9d85f865ab
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-15 14:16:09 +0500

    ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,14 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import { NavbarComponent } from "./Components/navbar/navbar.component";
 import { FootersComponent } from "./Components/footers/footers.component";
+import { RussiaComponent } from "./russia/russia.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, MatToolbarModule, MatIconModule, MatButtonModule, NavbarComponent, FootersComponent]
+    imports: [CommonModule, RouterOutlet, MatToolbarModule, MatIconModule, MatButtonModule, NavbarComponent, FootersComponent, RussiaComponent]
 })
 export class AppComponent {
   title = 'Exam-Project';
