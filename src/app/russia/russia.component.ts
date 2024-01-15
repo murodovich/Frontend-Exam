74d215ddfebdfa09c73d705321492fa4407a3c3b
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-15 14:15:46 +0500

    ts

---
+++ src/app/russia/russia.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-russia',
+  standalone: true,
+  imports: [],
+  templateUrl: './russia.component.html',
+  styleUrl: './russia.component.scss'
+})
+export class RussiaComponent {
+
+}
