1374616369e912a4702fea6e0adf4d6be9ab6739
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-15 15:51:45 +0500

    qwqw

---
+++ src/app/solutions/solutions.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-solutions',
+  standalone: true,
+  imports: [],
+  templateUrl: './solutions.component.html',
+  styleUrl: './solutions.component.scss'
+})
+export class SolutionsComponent {
+
+}
