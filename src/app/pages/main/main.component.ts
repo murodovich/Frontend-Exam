d8895265b977b194332c6c06a3964b00c958d122
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-17 20:18:56 +0500

    main

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { SolutionsComponent } from "../../Components/solutions/solutions.component";
+import { RussiaComponent } from "../../Components/russia/russia.component";
+import { RegistrComponent } from "../../Components/registr/registr.component";
+import { FootersComponent } from "../../Components/footers/footers.component";
+import { NavbarComponent } from "../../Components/navbar/navbar.component";
 
 @Component({
-  selector: 'app-main',
-  standalone: true,
-  imports: [],
-  templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+    selector: 'app-main',
+    standalone: true,
+    templateUrl: './main.component.html',
+    styleUrl: './main.component.scss',
+    imports: [SolutionsComponent, RussiaComponent, RegistrComponent, FootersComponent, NavbarComponent]
 })
 export class MainComponent {
 }
