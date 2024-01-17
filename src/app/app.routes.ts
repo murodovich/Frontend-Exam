1e240f14c40fbc523dc34f295f21083722926950
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-17 20:21:24 +0500

    asas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { NotFoundComponent } from './pages/notfound/not-found.component';
 import { MainComponent } from './pages/main/main.component';
+import { ProductComponent } from './pages/product/product.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { SubcategoryComponent } from './pages/subcategory/subcategory.component';
 
 export const routes: Routes = [
     {path:'main',component:MainComponent},
+    {path:'product',component:ProductComponent},
+    {path:'products',component:ProductsComponent},
+    {path:'subcategory',component: SubcategoryComponent},
+    {path:"",redirectTo:"main",pathMatch:'full'},
     {path:'**', component:NotFoundComponent}
+
 ];
  
