11caea4d18ee3745b7943e57818d1591209e8b32
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-17 20:20:16 +0500

    sasa

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../../Components/navbar/navbar.component";
+import { FootersComponent } from "../../Components/footers/footers.component";
+import { RegistrComponent } from "../../Components/registr/registr.component";
 
 @Component({
-  selector: 'app-product',
-  standalone: true,
-  imports: [],
-  templateUrl: './product.component.html',
-  styleUrl: './product.component.scss'
+    selector: 'app-product',
+    standalone: true,
+    templateUrl: './product.component.html',
+    styleUrl: './product.component.scss',
+    imports: [NavbarComponent, FootersComponent, RegistrComponent]
 })
 export class ProductComponent {
 
