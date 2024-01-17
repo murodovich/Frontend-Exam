15d48abf035796d2f0d22fdc8515211fbf7f6433
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-17 20:18:34 +0500

    navbar

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -1,17 +1,32 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,MatToolbarModule,MatIconModule,MatButtonModule],
+  imports: [CommonModule, RouterOutlet,MatToolbarModule,MatIconModule,MatButtonModule,RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
+  /**
+   *
+   */
+  constructor(private router: Router) {
+    
+  }
+
+  submit() {
+    this.router.navigate(['products']);
+  }
+
+  product() {
+    this.router.navigate(['product']);
+  }
 }
