e7b9affaf8724b39268273393176a0bdd7809fb0
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-18 17:08:46 +0500

    sasa

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -4,13 +4,14 @@ import { RussiaComponent } from "../../Components/russia/russia.component";
 import { RegistrComponent } from "../../Components/registr/registr.component";
 import { FootersComponent } from "../../Components/footers/footers.component";
 import { NavbarComponent } from "../../Components/navbar/navbar.component";
+import { NumbersComponent } from "../../Components/numbers/numbers.component";
 
 @Component({
     selector: 'app-main',
     standalone: true,
     templateUrl: './main.component.html',
     styleUrl: './main.component.scss',
-    imports: [SolutionsComponent, RussiaComponent, RegistrComponent, FootersComponent, NavbarComponent]
+    imports: [SolutionsComponent, RussiaComponent, RegistrComponent, FootersComponent, NavbarComponent, NumbersComponent]
 })
 export class MainComponent {
 }
