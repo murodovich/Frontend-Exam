a309894b8cf475a371b1ce05f1bc26054e6f1c6e
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-18 17:08:32 +0500

    vases

---
+++ src/app/Components/numbers/numbers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-numbers',
+  standalone: true,
+  imports: [],
+  templateUrl: './numbers.component.html',
+  styleUrl: './numbers.component.scss'
+})
+export class NumbersComponent {
+
+}
