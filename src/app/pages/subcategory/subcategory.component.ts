9149e46546e7e02ddd001738c2b106bee4e7ff31
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-18 17:09:32 +0500

    sasa

--- src/app/pages/subcategory/subcategory.component.ts
+++ src/app/pages/subcategory/subcategory.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../Components/navbar/navbar.component";
+import { FootersComponent } from "../../Components/footers/footers.component";
+import { NumbersComponent } from "../../Components/numbers/numbers.component";
 
 @Component({
     selector: 'app-subcategory',
     standalone: true,
     templateUrl: './subcategory.component.html',
     styleUrl: './subcategory.component.scss',
-    imports: [NavbarComponent]
+    imports: [NavbarComponent, FootersComponent, NumbersComponent]
 })
 export class SubcategoryComponent {
 
