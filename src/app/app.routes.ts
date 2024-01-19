ed080fec8f3ab1a1eae19abe5aad0825016f74e6
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-19 18:13:22 +0500

    subcategory

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,17 @@ import { MainComponent } from './pages/main/main.component';
 import { ProductComponent } from './pages/product/product.component';
 import { ProductsComponent } from './pages/products/products.component';
 import { SubcategoryComponent } from './pages/subcategory/subcategory.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { CompanyComponent } from './pages/company/company.component';
 
 export const routes: Routes = [
     {path:'main',component:MainComponent},
     {path:'product',component:ProductComponent},
     {path:'products',component:ProductsComponent},
     {path:'subcategory',component: SubcategoryComponent},
+    {path:'contact',component: ContactComponent},
     {path:"",redirectTo:"main",pathMatch:'full'},
+    {path:'company',component: CompanyComponent},
     {path:'**', component:NotFoundComponent}
 
 ];
