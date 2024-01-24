7825718306c8d7203e3c215ebe549f65ffd0341a
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-24 12:56:54 +0500

    sasas

---
+++ src/app/piprs.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { PiprsPipe } from './piprs.pipe';
+
+describe('PiprsPipe', () => {
+  it('create an instance', () => {
+    const pipe = new PiprsPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
