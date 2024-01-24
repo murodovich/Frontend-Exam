e5edad79111dac4e26e1576ede20ffe8585b4342
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-24 12:56:28 +0500

    asas

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,15 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from "../../Components/navbar/navbar.component";
 import { FootersComponent } from "../../Components/footers/footers.component";
 import { RegistrComponent } from "../../Components/registr/registr.component";
+import { ProductData } from '../../models/Product';
+import { ProductService } from '../../services/productServices/product.service';
+import { PiprsPipe } from "../../piprs.pipe";
 
 @Component({
     selector: 'app-product',
     standalone: true,
     templateUrl: './product.component.html',
     styleUrl: './product.component.scss',
-    imports: [NavbarComponent, FootersComponent, RegistrComponent]
+    imports: [NavbarComponent, FootersComponent, RegistrComponent, PiprsPipe]
 })
-export class ProductComponent {
 
+export class ProductComponent implements OnInit {
+    data!: ProductData;
+    constructor(
+        private service: ProductService) {
+
+    }
+    ngOnInit() {
+        this.getAllProduct();
+    }
+
+    getAllProduct() {
+        this.service.getAllProduct().subscribe(
+            (data: ProductData) => {
+                this.data = data;
+
+            })
+
+    }
 }
