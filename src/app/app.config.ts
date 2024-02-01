26c6eb3ae53057d915e649b625ead83b008b5825
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-02-01 14:55:31 +0500

    nksf

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
